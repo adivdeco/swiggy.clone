67411bb50c63ac626a220d60163101350dc119b7
adivdeco <sadiv120@gmail.com>
2025-03-22 23:32:55 +0530

    search done

--- src/Components/SearchItem.js
+++ src/Components/SearchItem.js
@@ -4,10 +4,11 @@ import Items from "./Items"
 
 
 export default function SearchItem({items , text}){
+console.log(text);
 
     // const [query , setquery] = useState("");
  
-    const filterData = items.filter((items)=> items.name.toLowerCase().includes('Margherita') )
+    const filterData = items.filter((items)=> items.name.toLowerCase().includes(text) )
     console.log(filterData);
     // const filterData = items.filter((item) => item.name.toLowerCase().includes(text));
     
